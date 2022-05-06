430c8539e10f22f94997f1c8e0320c73f3ab45a0
Ilya Fisenko <ilya.fisenko@waveaccess.ru>
2022-05-06 20:48:28 +0400

    Add api support

---
+++ lib/rest/retrofit.dart
@@ -0,0 +1,17 @@
+import 'package:dio/dio.dart';
+
+
+import '../model/response_token.dart';
+import 'apis.dart';
+
+part 'retrofit.g.dart';
+
+abstract class ApiRequest {
+  factory ApiRequest(Dio dio, {required String baseUrl}) = _ApiRequest;
+
+  Future<ResponseToken> signIn(String login, String password);
+
+  Future<ResponseToken> register(String login, String password);
+
+  Future<bool> hasAccess(String token);
+}
